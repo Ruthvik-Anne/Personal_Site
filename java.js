e0a18b64b7c94dd9461378e90ddde317cf544862
Ruthvik Anne <anneruthvik9@gmail.com>
2026-06-01 15:49:03 +0530

    Add auto-update for copyright year in footer

--- java.js
+++ java.js
@@ -1,1 +1,5 @@
-// JavaScript source code
+// Auto-update copyright year
+document.addEventListener("DOMContentLoaded", function () {
+  const f = document.querySelector(".footer-text");
+  if (f) f.textContent = "\u00a9 " + new Date().getFullYear() + " Ruthvik Anne";
+});
